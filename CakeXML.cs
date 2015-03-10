3bd0cfd3fa350f54e25f110b56b8a61d882af0a3
Frederic Guillot <fred@cakemail.com>
2015-03-10 13:45:25 -0400

    Add base64 encoding (https) instead of encryption

--- CakeXML.cs
+++ CakeXML.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Net;
@@ -11,307 +11,326 @@ using InOutDictionary = System.Collections.Generic.Dictionary<string, object>;
 // The C# library to access the API that CakeMail provided you is protected by the following licence: http://en.wikipedia.org/wiki/BSD_licenses
 namespace CakeLibrary
 {
-	public class CakeXML
-	{
-		protected string classes;
-		protected bool closed_xml;
-		protected int depth;
-		protected XmlDocument response;
-		protected string x_i;
-		protected string x_v;
-		protected string xml;
-		private CakeSettings _cakeSettings;
+    public class CakeXML
+    {
+        protected string classes;
+        protected bool closed_xml;
+        protected int depth;
+        protected XmlDocument response;
+        protected string x_i;
+        protected string x_v;
+        protected string xml;
+        private CakeSettings _cakeSettings;
 
-		public CakeXML(CakeSettings cakeSettings)
-		{
-			_cakeSettings = cakeSettings;
-		}
+        public CakeXML(CakeSettings cakeSettings)
+        {
+            _cakeSettings = cakeSettings;
+        }
 
-		/**
-		* Adds a new class into the xml' structure
-		*
-		* @class		the class' name
-		*/
-		public void AddClass(string className, string locale)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Adds a new class into the xml' structure
+        *
+        * @class        the class' name
+        */
+        public void AddClass(string className, string locale)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			Insert("<class type=\"" + HttpUtility.UrlPathEncode(className) + "\" locale=\"" + HttpUtility.UrlPathEncode(locale) + "\">");
-			depth += 2;
-		}
+            Insert("<class type=\"" + HttpUtility.UrlPathEncode(className) + "\" locale=\"" + HttpUtility.UrlPathEncode(locale) + "\">");
+            depth += 2;
+        }
 
-		/**
-		* Adds a new method to a class
-		*
-		* @method		the method's name
-		*/
-		public void AddMethod(string methodName)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Adds a new method to a class
+        *
+        * @method       the method's name
+        */
+        public void AddMethod(string methodName)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			Insert("<method type=\"" + HttpUtility.UrlPathEncode(methodName) + "\">");
-			depth += 2;
-		}
+            Insert("<method type=\"" + HttpUtility.UrlPathEncode(methodName) + "\">");
+            depth += 2;
+        }
 
-		/**
-	   * Adds a new parameter into a method
-	   *
-	   * @parameters	the parameters
-	   */
-		public void AddParameters(InOutDictionary parameters, bool reverseData)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+       * Adds a new parameter into a method
+       *
+       * @parameters    the parameters
+       */
+        public void AddParameters(InOutDictionary parameters, bool reverseData)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			foreach (KeyValuePair<string, object> parameter in parameters)
-			{
-				if (parameter.Value is InOutDictionary)
-				{
-					foreach (KeyValuePair<string, object> customParameter in (InOutDictionary)parameter.Value)
-					{
-						if (customParameter.Value is InOutDictionary)
-						{
-							AddTag(parameter.Key);
-							foreach (KeyValuePair<string, object> customParameter2 in (InOutDictionary)customParameter.Value)
-								AddCustomParameter(customParameter2.Key, customParameter2.Value);
-							CloseTag(parameter.Key);
-						}
-						else
-						{
-							if (reverseData) AddCustomParameter(customParameter.Value.ToString(), customParameter.Key);
-							else AddCustomParameter(customParameter.Key, customParameter.Value);
-						}
-					}
-				}
-				else if (parameter.Value is Array)
-				{
-					foreach (object customParameter in (Array)parameter.Value)
-					{
-						AddParameter(parameter.Key, customParameter);
-					}
-				}
-				else AddParameter(parameter.Key, parameter.Value);
-			}
-		}
+            foreach (KeyValuePair<string, object> parameter in parameters)
+            {
+                if (parameter.Value is InOutDictionary)
+                {
+                    foreach (KeyValuePair<string, object> customParameter in (InOutDictionary)parameter.Value)
+                    {
+                        if (customParameter.Value is InOutDictionary)
+                        {
+                            AddTag(parameter.Key);
+                            foreach (KeyValuePair<string, object> customParameter2 in (InOutDictionary)customParameter.Value)
+                                AddCustomParameter(customParameter2.Key, customParameter2.Value);
+                            CloseTag(parameter.Key);
+                        }
+                        else
+                        {
+                            if (reverseData) AddCustomParameter(customParameter.Value.ToString(), customParameter.Key);
+                            else AddCustomParameter(customParameter.Key, customParameter.Value);
+                        }
+                    }
+                }
+                else if (parameter.Value is Array)
+                {
+                    foreach (object customParameter in (Array)parameter.Value)
+                    {
+                        AddParameter(parameter.Key, customParameter);
+                    }
+                }
+                else AddParameter(parameter.Key, parameter.Value);
+            }
+        }
 
-		/**
-		* Adds a new parameter into a method
-		*
-		* @name		the parameter's name
-		* @value		the parameters's value
-		*/
-		public void AddParameter(string name, object value)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Adds a new parameter into a method
+        *
+        * @name     the parameter's name
+        * @value        the parameters's value
+        */
+        public void AddParameter(string name, object value)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			string val = value.ToString();
-			if (value is bool) val = val.ToLowerInvariant();
+            string val = value.ToString();
+            if (value is bool) val = val.ToLowerInvariant();
 
-			Insert("<" + name + ">" + EncodeContent(HttpUtility.UrlPathEncode(val)) + "</" + name + ">");
-		}
+            Insert("<" + name + ">" + EncodeContent(HttpUtility.UrlPathEncode(val)) + "</" + name + ">");
+        }
 
-		/**
-		* Adds a new custom parameter into a method
-		*
-		* @type		the parameter's type
-		* @value		the parameters's value
-		*/
-		public void AddCustomParameter(string type, object value)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Adds a new custom parameter into a method
+        *
+        * @type     the parameter's type
+        * @value        the parameters's value
+        */
+        public void AddCustomParameter(string type, object value)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			string val = value.ToString();
-			if (value is bool) val = val.ToLowerInvariant();
+            string val = value.ToString();
+            if (value is bool) val = val.ToLowerInvariant();
 
-			Insert("<data type=\"" + HttpUtility.HtmlEncode(HttpUtility.UrlPathEncode(type)) + "\">" + HttpUtility.HtmlEncode(HttpUtility.UrlPathEncode(val)) + "</data>");
-		}
+            Insert("<data type=\"" + HttpUtility.HtmlEncode(HttpUtility.UrlPathEncode(type)) + "\">" + HttpUtility.HtmlEncode(HttpUtility.UrlPathEncode(val)) + "</data>");
+        }
 
-		/**
-		* Adds a new tag
-		*/
-		public void AddTag(string tagName)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Adds a new tag
+        */
+        public void AddTag(string tagName)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			Insert("<" + tagName + ">");
-			depth += 2;
-		}
+            Insert("<" + tagName + ">");
+            depth += 2;
+        }
 
-		/**
-		* Closes the xml' structure
-		*/
-		public void Close()
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Closes the xml' structure
+        */
+        public void Close()
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			depth -= 2;
-			Insert("</body>");
+            depth -= 2;
+            Insert("</body>");
 
-			closed_xml = true;
-		}
+            closed_xml = true;
+        }
 
-		/**
-		* Closes a class
-		*/
-		public void CloseClass()
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Closes a class
+        */
+        public void CloseClass()
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			depth -= 2;
-			Insert("</class>");
-		}
+            depth -= 2;
+            Insert("</class>");
+        }
 
-		/**
-		* Closes a method
-		*/
-		public void CloseMethod()
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Closes a method
+        */
+        public void CloseMethod()
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			depth -= 2;
-			Insert("</method>");
-		}
+            depth -= 2;
+            Insert("</method>");
+        }
 
-		/**
-		* Closes a tag
-		*/
-		public void CloseTag(string tagName)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Closes a tag
+        */
+        public void CloseTag(string tagName)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			depth -= 2;
-			Insert("</" + tagName + ">");
-		}
+            depth -= 2;
+            Insert("</" + tagName + ">");
+        }
 
-		/**
-		* Executes the xml
-		*/
-		public void ExecXML()
-		{
-			if (!closed_xml) throw new CakeException(CakeErrors.UNCLOSED_XML);
+        /**
+        * Executes the xml
+        */
+        public void ExecXML()
+        {
+            string xml_req = "";
+            string IV = "";
+            CakeCrypt cakeCrypt = null;
 
-			DateTime startTime = DateTime.Now;
+            if (!closed_xml) throw new CakeException(CakeErrors.UNCLOSED_XML);
 
-			CakeDebug.AddMessage(xml, _cakeSettings);
+            DateTime startTime = DateTime.Now;
 
-			CakeCrypt cakeCrypt = new CakeCrypt(_cakeSettings.CAKE_MCRYPT_ALG, _cakeSettings.CAKE_MCRYPT_MODE, _cakeSettings.CAKE_INTERFACE_KEY);
+            CakeDebug.AddMessage(xml, _cakeSettings);
 
-			cakeCrypt.cryptor.IV = Encoding.Default.GetBytes("00000000");
-			string xml_req = cakeCrypt.CakeEncryptHex(xml);
+            if (_cakeSettings.CAKE_MCRYPT_ALG == "base64")
+            {
+                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(xml);
+                xml_req = System.Convert.ToBase64String(plainTextBytes);
+            }
+            else
+            {
+                cakeCrypt = new CakeCrypt(_cakeSettings.CAKE_MCRYPT_ALG, _cakeSettings.CAKE_MCRYPT_MODE, _cakeSettings.CAKE_INTERFACE_KEY);
 
-			string IV = "";
-			if (cakeCrypt.cryptor.Mode != CipherMode.ECB) IV = CakeCrypt.ToHex(cakeCrypt.cryptor.IV);
+                cakeCrypt.cryptor.IV = Encoding.Default.GetBytes("00000000");
+                xml_req = cakeCrypt.CakeEncryptHex(xml);
 
-			using (WebClient webClient = new WebClient())
-			{
-				webClient.Headers.Add("user-agent", "CakeLibrary .NET " + _cakeSettings.CAKE_VERSION);
+                if (cakeCrypt.cryptor.Mode != CipherMode.ECB) IV = CakeCrypt.ToHex(cakeCrypt.cryptor.IV);
+            }
 
-				NameValueCollection data = new NameValueCollection();
-				data.Add("id", _cakeSettings.CAKE_INTERFACE_ID.ToString());
-				data.Add("alg", _cakeSettings.CAKE_MCRYPT_ALG);
-				data.Add("mode", _cakeSettings.CAKE_MCRYPT_MODE);
-				data.Add("request", IV + xml_req);
-				CakeDebug.AddMessage(IV + xml_req, _cakeSettings);
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Headers.Add("user-agent", "CakeLibrary .NET " + _cakeSettings.CAKE_VERSION);
 
-				string resXML = Encoding.Default.GetString(webClient.UploadValues(_cakeSettings.API_CAKE_URL, "POST", data));
+                NameValueCollection data = new NameValueCollection();
+                data.Add("id", _cakeSettings.CAKE_INTERFACE_ID.ToString());
+                data.Add("alg", _cakeSettings.CAKE_MCRYPT_ALG);
+                data.Add("mode", _cakeSettings.CAKE_MCRYPT_MODE);
 
-				resXML = resXML.Contains("<?xml") ? resXML.Substring(resXML.IndexOf("<?xml")) : cakeCrypt.CakeDecryptHex(resXML);
+                if (_cakeSettings.CAKE_MCRYPT_ALG == "base64")
+                {
+                    data.Add("request", xml_req);
+                    CakeDebug.AddMessage(xml_req, _cakeSettings);
+                }
+                else
+                {
+                    data.Add("request", IV + xml_req);
+                    CakeDebug.AddMessage(IV + xml_req, _cakeSettings);
+                }
 
-				CakeDebug.AddMessage(resXML, _cakeSettings);
+                string resXML = Encoding.Default.GetString(webClient.UploadValues(_cakeSettings.API_CAKE_URL, "POST", data));
+                resXML = resXML.Contains("<?xml") ? resXML.Substring(resXML.IndexOf("<?xml")) : cakeCrypt.CakeDecryptHex(resXML);
 
-				response = new XmlDocument();
-				response.LoadXml(resXML);
-			}
+                CakeDebug.AddMessage(resXML, _cakeSettings);
 
-			DateTime endTime = DateTime.Now;
+                response = new XmlDocument();
+                response.LoadXml(resXML);
+            }
 
-			CakeDebug.AddMessage("Spent time: " + (endTime - startTime) + " s \r\n", _cakeSettings);
-		}
+            DateTime endTime = DateTime.Now;
 
-		/**
-		* Returns the xml structure
-		*/
-		public string GetXML()
-		{
-			return xml;
-		}
+            CakeDebug.AddMessage("Spent time: " + (endTime - startTime) + " s \r\n", _cakeSettings);
+        }
 
-		/**
-		* Returns the XML response
-		*/
-		public XmlDocument GetResponse()
-		{
-			return response;
-		}
+        /**
+        * Returns the xml structure
+        */
+        public string GetXML()
+        {
+            return xml;
+        }
 
-		/**
-		* Initialises the xml' structure
-		*/
-		public void Init()
-		{
-			if (xml == null)
-			{
-				Insert("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-				Insert("<body version=\"" + _cakeSettings.CAKE_VERSION + "\">");
-				depth += 2;
-			}
-		}
+        /**
+        * Returns the XML response
+        */
+        public XmlDocument GetResponse()
+        {
+            return response;
+        }
 
-		/**
-		* Insert a new line with identation
-		*
-		* @value
-		* @returns	an exception in case of error
-		*/
-		protected void Insert(string value)
-		{
-			if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
+        /**
+        * Initialises the xml' structure
+        */
+        public void Init()
+        {
+            if (xml == null)
+            {
+                Insert("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+                Insert("<body version=\"" + _cakeSettings.CAKE_VERSION + "\">");
+                depth += 2;
+            }
+        }
 
-			for (int i = 0; i < depth; i++) xml += " ";
+        /**
+        * Insert a new line with identation
+        *
+        * @value
+        * @returns  an exception in case of error
+        */
+        protected void Insert(string value)
+        {
+            if (closed_xml) throw new CakeException(CakeErrors.CLOSED_XML);
 
-			xml += value + "\n";
-		}
+            for (int i = 0; i < depth; i++) xml += " ";
 
-		/**
-		* Encode a string into a HTML safe string
-		*/
-		private static String EncodeContent(string str)
-		{
-			if (string.IsNullOrEmpty(str)) return "";
+            xml += value + "\n";
+        }
 
-			StringBuilder output = new StringBuilder((int)(str.Length * 1.1));
+        /**
+        * Encode a string into a HTML safe string
+        */
+        private static String EncodeContent(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return "";
 
-			foreach (char c in str)
-			{
-				switch (c)
-				{
-					case '&':
-						output.Append("&amp;");
-						break;
-					case '>':
-						output.Append("&gt;");
-						break;
-					case '<':
-						output.Append("&lt;");
-						break;
-					case '"':
-						output.Append("&quot;");
-						break;
-					default:
-						Int32 iCharValue = Convert.ToInt32(c);
-						if (iCharValue > 127)
-						{
-							output.Append("&#");
-							output.Append(iCharValue.ToString());
-							output.Append(";");
-						}
-						else
-						{
-							output.Append(c);
-						}
-						break;
-				}
-			}
-			return output.ToString();
-		}
-	}
+            StringBuilder output = new StringBuilder((int)(str.Length * 1.1));
+
+            foreach (char c in str)
+            {
+                switch (c)
+                {
+                    case '&':
+                        output.Append("&amp;");
+                        break;
+                    case '>':
+                        output.Append("&gt;");
+                        break;
+                    case '<':
+                        output.Append("&lt;");
+                        break;
+                    case '"':
+                        output.Append("&quot;");
+                        break;
+                    default:
+                        Int32 iCharValue = Convert.ToInt32(c);
+                        if (iCharValue > 127)
+                        {
+                            output.Append("&#");
+                            output.Append(iCharValue.ToString());
+                            output.Append(";");
+                        }
+                        else
+                        {
+                            output.Append(c);
+                        }
+                        break;
+                }
+            }
+            return output.ToString();
+        }
+    }
 }
